728d8038df04a29f1263790343ea298596d92587
itochs <halglobe0108@gmail.com>
2023-04-19 16:35:48 +0900

    importの確認

--- score/move_score.py
+++ score/move_score.py
@@ -1,5 +1,10 @@
+from section_divide import get_section
+from pprint import pprint
+
+
 def main():
-    pass
+    notes_section = get_section("score/data/m155_notes.json")
+    pprint(notes_section[0])
 
 
 if __name__ == "__main__":
