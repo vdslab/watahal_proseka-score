4293d6608346478d556106d4dfe5cbba29e02891
itochs <halglobe0108@gmail.com>
2023-04-19 17:22:41 +0900

    ノーツのindexを時間ごとに格納

--- score/move_score.py
+++ score/move_score.py
@@ -1,10 +1,18 @@
 from section_divide import get_section
 from pprint import pprint
+from collections import defaultdict
+import constant
 
 
 def main():
     notes_section = get_section("score/data/m155_notes.json")
     pprint(notes_section[0])
+    section = notes_section[0]
+    notes_index_by_y = defaultdict(list[int])
+    for i, note in enumerate(section):
+        notes_index_by_y[note["y"]].append(i)
+    for note in notes_index_by_y.items():
+        print(note)
 
 
 if __name__ == "__main__":
