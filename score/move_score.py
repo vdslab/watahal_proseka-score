0cc0c0899c0c10e46b180fb91d4a9b5e7ff98e9a
itochs <halglobe0108@gmail.com>
2023-04-26 14:15:29 +0900

    ホールドの考慮

--- score/move_score.py
+++ score/move_score.py
@@ -14,10 +14,10 @@ def main():
     notes_section = get_section("score/data/m155_notes.json")
     for i, section in enumerate(notes_section):
         # --- デバッグ用の区間調整
-        if i < 3:
-            continue
-        if i >= 8:
-            break
+        # if i < 3:
+        #     continue
+        # if i >= 8:
+        #     break
         # ---
         print(section)
         # y座標ごとのノーツIDを格納
@@ -52,23 +52,27 @@ def main():
 
             return True
 
-        def update_cost_from_index(
+        def update_hand_from_index(
             section: list[list[dict]], hand: dict, note_index: int
         ) -> dict:
             push_note = section[note_index]
             hand["notes"].append(note_index)
             hand["cost"] += abs(hand["x"] - push_note["x"])
             hand["x"] = push_note["x"]
+            if push_note["hold_type"] == "end":
+                hand["pushing"] = False
+            else:
+                hand["pushing"] = True
 
         # 運指の作成
         for note in notes_index_by_y.items():
             if len(note[1]) >= 2:
                 # 単純に左右にある方をそれぞれに追加
                 left_note_index = note[1][0]
-                update_cost_from_index(section, left, left_note_index)
+                update_hand_from_index(section, left, left_note_index)
 
                 right_note_index = note[1][1]
-                update_cost_from_index(section, right, right_note_index)
+                update_hand_from_index(section, right, right_note_index)
             else:
                 # 移動距離をそれぞれ求めて，小さい方で取る
                 note_index = note[1][0]
@@ -77,17 +81,27 @@ def main():
 
                 if left_move <= right_move:
                     if can_push_note(left, note_index):
-                        update_cost_from_index(section, left, note_index)
+                        update_hand_from_index(section, left, note_index)
                     else:
-                        update_cost_from_index(section, right, note_index)
+                        update_hand_from_index(section, right, note_index)
                 else:
                     if can_push_note(left, note_index):
-                        update_cost_from_index(section, right, note_index)
+                        update_hand_from_index(section, right, note_index)
                     else:
-                        update_cost_from_index(section, left, note_index)
+                        update_hand_from_index(section, left, note_index)
+
+        # --- デバッグ用
+        # 出力確認
+        print("i:", i)
+        print("left")
         print(left)
+        print("right")
         print(right)
         print()
+        # 区間調整
+        # if i >= 64:
+        #     break
+        # ---
 
 
 if __name__ == "__main__":
