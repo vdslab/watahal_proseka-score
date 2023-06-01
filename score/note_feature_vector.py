541dd396f6d9dcd9ed949185f20b337754a3e36e
itochs <halglobe0108@gmail.com>
2023-06-01 12:22:48 +0900

    特徴量の各変数を追加

--- score/note_feature_vector.py
+++ score/note_feature_vector.py
@@ -4,7 +4,16 @@ from section_divide import get_section
 
 
 def _get_feature_vector(section: list[dict]):
-    pass
+    # 曲特徴
+    duration = 0
+    note_density = 0
+    note_types_count = dict()
+    # x_shift：右にだんだんとずれていく=>値が大きい
+    x_shift = 0
+    consecutive_same_x_count_list = []
+    # 運指特徴
+    hand_balance = 0
+    distances_between_notes_list = []
 
 
 def get_feature_vectors(notes_json_file_relative_path: str):
