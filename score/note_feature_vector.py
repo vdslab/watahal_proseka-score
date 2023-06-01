5197387a08398599e866ff1871d3a1c1ff171f3a
itochs <halglobe0108@gmail.com>
2023-06-01 12:07:31 +0900

    全体像の作成

--- score/note_feature_vector.py
+++ score/note_feature_vector.py
@@ -1,6 +1,22 @@
-def get_feature_vector():
+from pprint import pprint
+
+from section_divide import get_section
+
+
+def _get_feature_vector(section: list[dict]):
     pass
+
+
+def get_feature_vectors(notes_json_file_relative_path: str):
+    notes_sections = get_section("score/data/m155_notes.json")
+    feature_vectors = []
+    for section in notes_sections:
+        vector = _get_feature_vector(section)
+        feature_vectors.append(vector)
+
+    return feature_vectors
 
 
 if __name__ == "__main__":
-    pass
+    feature_vectors = get_feature_vectors()
+    pprint(feature_vectors)
