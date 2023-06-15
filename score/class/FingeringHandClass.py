6ddb3eb2dadfc3a480b4fff1ee91830d2b69c993
itochs <halglobe0108@gmail.com>
2023-06-15 13:09:58 +0900

    format

--- score/class/FingeringHandClass.py
+++ score/class/FingeringHandClass.py
@@ -8,13 +8,15 @@ class FingeringHand:
         self.notes = []
         self.cost = 0
         self.pushing = False
-    
+
     def update_move_cost(self, to_x: int) -> None:
         if MAX_KEYBOARD_COUNT <= to_x:
-            print(f"[WARNING] get x:{to_x} is over max keyboard length ( {MAX_KEYBOARD_COUNT} ). cannot update cost")
+            print(
+                f"[WARNING] get x:{to_x} is over max keyboard length ( {MAX_KEYBOARD_COUNT} ). cannot update cost"
+            )
             return
-        
+
         self.cost = abs(self._x - to_x)
-    
+
     def can_push(self) -> bool:
         pass
