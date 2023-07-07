3532888ab7cc92051d3aee5ce49598ac7d9d1d4d
watoson <chwa20058@g.nihon-u.ac.jp>
2023-07-07 15:50:47 +0900

    jsonデータ関連の追加

---
+++ proseka/getjson.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+urls = list()
+names = list()
+for number in range(1,328):
+    urls.append('https://proseka-trainer.com/music_scores/' + str(number).zfill(3) + '_m.json')
+    names.append('song' + str(number))
+# print(urls)
+# print(names)
+for i in range(0,327):
+    data = requests.get(urls[i]).json()
+    with open('datas/' + names[i] + '.json',mode='w') as f:
+        json.dump(data,f)
