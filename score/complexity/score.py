1028c84f2a343871b7a93976ba326bf9c324ef0a
itochs <halglobe0108@gmail.com>
2023-10-30 08:18:51 +0900

    複雑さの方で試す

--- score/complexity/score.py
+++ score/complexity/score.py
@@ -43,6 +43,7 @@ from pprint import pprint
 
 import numpy as np
 from more_itertools import windowed
+from speed import get_duration_weighted_average_bpm
 from vis.bpm import get_bpm_change2
 from vis.density import get_y_densities
 
@@ -100,7 +101,15 @@ if __name__ == "__main__":
         score_file_paths, key=lambda path: int(path.split(".")[0].split("-")[1])
     )
 
-    for path in score_file_paths[150:160]:
+    score_info = []
+    score_status = []
+
+    # detail
+    details = None
+    with open("proseka/datas/detail/data.json") as f:
+        details: list[dict] = json.load(f)
+
+    for path in score_file_paths:
         # flickが含まれる
         normal_notes = get_normal_notes(path)
         x_diffs = get_x_diff_rates(normal_notes)
@@ -108,27 +117,55 @@ if __name__ == "__main__":
         # めっちゃぶれてた方がわかりやすいので平均値は大きい方がいい
         # そのぶれ方は，全体的に散ってた方がいいので標準偏差は小さい方がいい？ので逆数
         # かけ算だとそれっぽいので1以下にならないように1を足す
-        x_diff_status = np.mean(x_diffs) * (1 + 1 / np.std(x_diffs))
+        # x_diff_status = np.mean(x_diffs) * (1 / np.std(x_diffs))
+        x_diff_status = np.mean(x_diffs) * np.std(x_diffs)
 
         id = int(path.split(".")[0].split("-")[1])
+        weighted_bpm = get_duration_weighted_average_bpm(f"proseka/datas/song{id}.json")
+
         # 値が大きいほど変化が急なので単純でない．そのため逆数をとる
         bpm_change = get_bpm_change2(f"proseka/datas/song{id}.json")
-        bpm_change_status = 1 / (1 + bpm_change) + 1
+        # bpm_change_status = 1 / (np.log(1 + bpm_change) + 1)
+        bpm_change_status = np.log(1 + bpm_change) + 1
 
-        y_densities = get_y_densities(path)
+        y_densities = get_y_densities(path, separate_measure=1) * weighted_bpm
         # 密度は低ければ低いほど単純
         # そのばらつきも低いほど単純
-        y_densities_status = (1 + 1 / np.mean(y_densities)) * (
-            1 + 1 / np.std(y_densities)
+        # ただしBPMが高いと単純でなくなる
+        # y_densities_status = 1 / (np.mean(y_densities) * np.std(y_densities))
+        y_densities_status = np.mean(y_densities) * np.std(y_densities)
+
+        status = (
+            x_diff_status * bpm_change_status * y_densities_status * (1 / weighted_bpm)
         )
 
-        # x_diff_stats = [np.mean(x_diffs), np.std(x_diffs)]
-        # pprint(x_diff_stats)
-        # print(f"{x_diff_status=}")
-        # print(f"{bpm_change=}", f"{bpm_change_status=}")
-        # pprint(y_densities)
-        # print(f"{y_densities_status=}")
+        cur_detail = list(filter(lambda detail: detail["id"] == id, details))[0]
 
-        status = x_diff_status * bpm_change_status * y_densities_status
-        print(path, f"{status=}")
-        print()
+        info = dict()
+        info["id"] = id
+        info["name"] = cur_detail["name"]
+        info["level"] = cur_detail["level"]
+        info["status"] = status
+        # info["x_diff_stats"] = x_diff_stats
+        # info["bpm_change_status"] = bpm_change_status
+        # info["weighted_bpm"] = weighted_bpm
+        # info["y_densities_stats"] = y_densities_stats
+        score_status.append(info)
+
+    # pprint(score_status)
+
+    score_status = sorted(score_status, key=lambda info: info["status"], reverse=True)
+    pprint(score_status)
+
+    details = sorted(details, key=lambda detail: detail["level"])
+    cnt_by_level = dict()
+    for detail in details:
+        level = detail["level"]
+        if level not in cnt_by_level:
+            cnt_by_level[level] = 0
+        cnt_by_level[level] += 1
+
+    in_cnt_by_level = dict()
+    for i, data in enumerate(score_status):
+        level = data["level"]
+        # if i < cnt_by_level[level]
