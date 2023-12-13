613922d2172bfb1fe07f605a5f9ce6509f1b69f8
itochs <halglobe0108@gmail.com>
2023-12-14 01:01:43 +0900

    運指取得

---
+++ score/main__fingering.py
@@ -0,0 +1,32 @@
+import glob
+import json
+import os
+import re
+from pprint import pp, pprint
+
+from fingering.move_score import get_fingering
+from term_printer import Color, cprint
+
+file_paths = glob.glob("./proseka/datas/*.json")
+file_paths = sorted(file_paths, key=lambda path: int(re.search(r"\d+", path).group()))
+save_dir = "./score/data/_json/1213/fingering"
+os.makedirs(save_dir, exist_ok=True)
+for path in file_paths:
+    cprint(f"{path=}", attrs=[Color.GREEN])
+    fingering = get_fingering(path)
+    # print(fingering)
+    # print(len(fingering["left"]), len(fingering["right"]))
+    fingering_notes_count = len(fingering["left"]) + len(fingering["right"])
+    data_notes_count = 0
+    id = int(re.search(r"\d+", path).group())
+    with open(f"score/data/notes_score/score-{id}.json", "r") as f:
+        notes = json.load(f)
+        data_notes_count = len(notes)
+
+    # assert (
+    #     fingering_notes_count == data_notes_count
+    # ), f"{fingering_notes_count=}, {data_notes_count=}"
+
+    name = os.path.splitext(os.path.basename(path))[0]
+    with open(f"{save_dir}/{name}.json", "w", newline="") as f:
+        json.dump(fingering, f, indent=2, ensure_ascii=False)
